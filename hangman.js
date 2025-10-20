8d113bb617ce3749ed380d70358ffd758ccd44ea
Dayana Hernandez <dayanahern15@gmail.com>
2025-10-20 10:27:06 -0400

    initial word lenght clue (needs fixing)

--- hangman.js
+++ hangman.js
@@ -1,4 +1,28 @@
 var POSSIBLE_WORDS= ["obdurate","verisimilitude","defenestrate","obsequious","dissonant","toady","idempotent"];
 
-var randomIndex = parseInt(Math.random()*POSSIBLE_WORDS.length);
-var word = POSSIBLE_WORDS[randomIndex]
+
+var word = "";
+
+function newGame() {
+    var randomIndex = parseInt(Math.random() * POSSIBLE_WORDS.length);
+    word = POSSIBLE_WORDS[randomIndex];
+
+    var clueString = "";
+    for (var i = 0 ; i < word.length; i++)
+    {
+        clueString += "_ ";
+    }
+
+    var clue = document.getElementById("Clue");
+    clue.innerHTML = clueString;
+
+    function guess(){
+        updatePage();
+    }
+
+    function updatePage(){
+        
+    }
+
+}
+
