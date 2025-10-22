62a5b3bae9d9d377a5a8f1013324a453437920a3
Dayana Hernandez <dayanahern15@gmail.com>
2025-10-22 09:57:00 -0400

    updated image functionality

--- hangman.js
+++ hangman.js
@@ -1,18 +1,25 @@
 var POSSIBLE_WORDS =["obduraate", "verismilitude", "defenestrate",
     "obsequious", "dissonant", "today", "idempotent"]
+    var MAX_GUESSES = 6;
     var word ="";
     var guesses = "";
+    var guess_count = MAX_GUESSES;
 
 function newGame(){
      var randomIndex = parseInt(Math.random() * POSSIBLE_WORDS.length);
      word = POSSIBLE_WORDS[randomIndex];
     guesses = " ";
+    guess_count = MAX_GUESSES;
     updatePage();
 }
 function guessLetter(){
 
     var input = document.getElementById("guess");
     var letter = input.value;
+    if(word.indexOf(letter)< 0)
+    {
+        guess_count--;
+    }
     guesses+=letter;
     updatePage();
 } 
@@ -27,9 +34,18 @@ function updatePage(){
         else
             clueString+= "_ ";
         }
+    
+    //clue string
     var clue = document.getElementById("clue");
     clue.innerHTML = clueString;
 
+    //update the guesses from the user
     var guessArea = document.getElementById("guesses");
     guessArea.innerHTML = "Guessed Letters: " + guesses;
+
+    //update the images
+    var image = document.getElementById("hangmanImage");
+    image.src = "images/hangman" + guess_count + ".gif"
 }
+
+
