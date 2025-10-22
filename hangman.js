e8a6031cd659995b6b2aed160de20c58dadfc266
Dayana Hernandez <dayanahern15@gmail.com>
2025-10-22 09:40:03 -0400

    updated

--- hangman.js
+++ hangman.js
@@ -1,44 +1,35 @@
-var POSSIBLE_WORDS= ["obdurate","verisimilitude","defenestrate","obsequious","dissonant","toady","idempotent"];
+var POSSIBLE_WORDS =["obduraate", "verismilitude", "defenestrate",
+    "obsequious", "dissonant", "today", "idempotent"]
+    var word ="";
+    var guesses = "";
 
-
-var word = "";
-var guesses = "";
-
-function newGame() {
-    var randomIndex = parseInt(Math.random() * POSSIBLE_WORDS.length);
-    word = POSSIBLE_WORDS[randomIndex];
-    guesses = "";
+function newGame(){
+     var randomIndex = parseInt(Math.random() * POSSIBLE_WORDS.length);
+     word = POSSIBLE_WORDS[randomIndex];
+    guesses = " ";
     updatePage();
 }
-
 function guessLetter(){
+
     var input = document.getElementById("guess");
     var letter = input.value;
-    guesses += letter;
+    guesses+=letter;
     updatePage();
-
-}
-
+} 
 function updatePage(){
-
-    var clueString = "";
-    for (var i = 0 ; i < word.length; i++)
-    {
+     var clueString = "";
+     for(var i = 0; i <word.length; i++){
+        var currentLetter = word.charAt(i);
         
-
-        clueString += "_ ";
-    }
-
-    var clue = document.getElementById("Clue");
+        if(guesses.indexOf(currentLetter) >= 0){
+            clueString += currentLetter + " ";
+        }
+        else
+            clueString+= "_ ";
+        }
+    var clue = document.getElementById("clue");
     clue.innerHTML = clueString;
 
-    function guess(){
-        updatePage();
-    }
-
-    function updatePage(){
-
-    }
-
+    var guessArea = document.getElementById("guesses");
+    guessArea.innerHTML = "Guessed Letters: " + guesses;
 }
-
